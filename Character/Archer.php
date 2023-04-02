c9b3d27fe9c69b1730bd1d4aa531c3c0085135e7
quentiin19 <quentin.bosseboeuf@gmail.com>
2023-04-02 12:13:00 +0200

    Arborescense

---
+++ Character/Archer.php
@@ -0,0 +1,35 @@
+<?php
+
+class Archer extends Character
+{
+    public function __construct(
+        float $health,
+        float $defenseRatio,
+        int $attackDamages,
+        int $magicDamages,
+    )
+    {
+        if ($magicDamages > $attackDamages) {
+            throw new Exception("The archer cannot have more magic damages than physical damages.");
+        }
+        parent::__construct($health, $defenseRatio, $attackDamages, $magicDamages);
+    }
+
+    public function getAttackDamages(): float
+    {
+        if (chance(20)) {
+            // echo "Coup critique !".PHP_EOL;
+            return $this->attackDamages*1.2;
+        }
+        return parent::getAttackDamages();
+    }
+
+    public function getDefenseRatio()
+    {
+        if (chance(10)) {
+            // echo "Esquive !".PHP_EOL;
+            return 1;
+        }
+        return parent::getDefenseRatio();
+    }
+}
