00a66048eaee6271b829540b24f7d5e63a2ed816
quentiin19 <quentin.bosseboeuf@gmail.com>
2023-04-02 11:41:12 +0200

    hasweapon

---
+++ HasWeapon.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use git Weapon;
+
+trait HasWeapon
+{
+
+    public function takesWeapon(Weapon $weapon)
+    {
+        // echo "{$this} ramasse {$weapon->name} !".PHP_EOL;
+        $this->weapon = $weapon;
+    }
+
+    public function dropsWeapon()
+    {
+        if ($this->hasWeapon()) {
+            // echo "{$this} fait tomber {$this->weapon->name}…".PHP_EOL;
+            $this->weapon = null;
+        }
+    }
+
+    public function hasWeapon()
+    {
+        return $this->weapon instanceof Weapon;
+    }
+}
