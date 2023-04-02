9998a3c8605a217e7e27f4eb468695174bdc9d0f
Yannis Zribi <yanis.zribi@gmail.com>
2023-04-02 12:09:00 +0200

    ajout du trait Type

---
+++ Type.php
@@ -0,0 +1,78 @@
+<?php
+
+
+Trait Type{
+
+
+    public function advantage(string $attackeeType, string $attackerType){
+        switch ($attackerType) {
+            case 'Feu':
+                switch ($attackeeType) {
+                    case 'Feu':
+                        return 1;
+                        break;
+                
+                    case 'Eau':
+                        return 0.75;
+                        break;
+                
+                    case 'Plante':
+                        return 1.25;
+                        break;
+                        
+                    default:
+                        print("le type passé en paramètre \$attackerType->({$attackerType}) à advantage() ne semble pas existé".PHP_EOL);
+                        return 0;
+                        break;
+                }
+                break;
+        
+            case 'Eau':
+                switch ($attackeeType) {
+                    case 'Feu':
+                        return 1.3;
+                        break;
+                
+                    case 'Eau':
+                        return 1;
+                        break;
+                
+                    case 'Plante':
+                        return 0.7;
+                        break;
+                        
+                    default:
+                        print("le type passé en paramètre \$attackerType->({$attackerType}) à advantage() ne semble pas existé".PHP_EOL);
+                        return 0;
+                        break;
+                }
+                break;
+        
+            case 'Plante':
+                switch ($attackeeType) {
+                    case 'Feu':
+                        return 0.8;
+                        break;
+                
+                    case 'Eau':
+                        return 1.2;
+                        break;
+                
+                    case 'Plante':
+                        return 1;
+                        break;
+                        
+                    default:
+                        print("le type passé en paramètre \$attackerType->({$attackerType}) à advantage() ne semble pas existé".PHP_EOL);
+                        return 0;
+                        break;
+                }
+                break;
+                
+            default:
+                print("le type passé en paramètre \$attackerType->({$attackerType}) à advantage() ne semble pas existé".PHP_EOL);
+                return 0;
+                break;
+        }
+    }
+}
